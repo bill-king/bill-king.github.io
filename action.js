3b0c65962b61d3e6cf1ab374fe43f92600602463
jon <jon@jons-MacBook-Pro-2.local>
2019-09-07 13:16:23 -0700

    jq

--- action.js
+++ action.js
@@ -35,14 +35,31 @@ $("#about-btn").click(function () {
 var $form = $('form#register-form'),
     url = 'https://script.google.com/macros/s/AKfycbwacUQ5UKYoWp-789IIqSPeMq9Iqr0xwhFAGb_2ovurqBMHa4k/exec'
 
-$('#submit-form').on('click', function(e) {
-  e.preventDefault();
-  var jqxhr = $.ajax({
-    url: url,
-    method: "GET",
-    dataType: "json",
-    data: $form.serializeObject()
-  }).success(
-    // do something
-  );
+
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || "");
+        } else {
+            o[this.name] = this.value || "";
+        }
+    });
+    return o;
+};
+
+$('#submit-form').on('click', function (e) {
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: $form.serializeObject()
+    }).success(
+        // do something
+    );
 })
